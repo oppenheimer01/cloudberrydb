bea9c7925d92f869b0ad1a96436e452b7cb03d79
HuSen <husen.xjtu@gmail.com>
2023-11-03 17:53:24 +0800

    Add: new hook to get control in getgpsegmentCount. (#277)
    New hook 'getgpsegmentCount_hook' for plugins to get control in getgpsegmentCount.

--- src/backend/cdb/cdbutil.c
+++ src/backend/cdb/cdbutil.c
@@ -80,6 +80,11 @@
 MemoryContext CdbComponentsContext = NULL;
 static CdbComponentDatabases *cdb_component_dbs = NULL;
 
+/*
+ * Hook for plugins to get control in getgpsegmentCount.
+ */
+getgpsegmentCount_hook_type getgpsegmentCount_hook = NULL;
+
 #ifdef USE_INTERNAL_FTS
 
 /*
@@ -1877,6 +1882,9 @@ getgpsegmentCount(void)
 {
 	/* 1 represents a singleton postgresql in utility mode */
 	int32 numsegments = 1;
+
+	if (getgpsegmentCount_hook)
+		return (*getgpsegmentCount_hook)();
 
 	if (Gp_role == GP_ROLE_DISPATCH)
 		numsegments = cdbcomponent_getCdbComponents()->total_segments;
@@ -4098,6 +4106,9 @@ getgpsegmentCount(void)
 {
 	/* 1 represents a singleton postgresql in utility mode */
 	int32 numsegments = 1;
+
+	if (getgpsegmentCount_hook)
+		return (*getgpsegmentCount_hook)();
 
 	if (Gp_role == GP_ROLE_DISPATCH)
 		numsegments = cdbcomponent_getCdbComponents()->total_segments;
--- src/include/cdb/cdbutil.h
+++ src/include/cdb/cdbutil.h
@@ -52,7 +52,13 @@ typedef enum SegmentType
 }SegmentType;
 
 /*
- * performs all necessary setup required for initializing Apache Cloudberry components.
+ * Hook for plugins to get control in getgpsegmentCount.
+ */
+typedef int (*getgpsegmentCount_hook_type)(void);
+extern PGDLLIMPORT getgpsegmentCount_hook_type getgpsegmentCount_hook;
+
+/*
+ * performs all necessary setup required for initializing Cloudberry Database components.
  *
  * This includes cdblink_setup() and initializing the Motion Layer.
  *
