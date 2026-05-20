49d8b667a5e1ef51918317ee1f0a2d1ebf86a423
Jinbao Chen <chenjinbao1989@gmail.com>
2026-05-20 10:16:01 -0400

    test

--- contrib/pax_storage/sql/pax_inject_fault.sql
+++ contrib/pax_storage/sql/pax_inject_fault.sql
@@ -2,10 +2,10 @@ create table t_insert(a int);
 select gp_inject_fault_infinite('fts_probe','skip',dbid) FROM gp_segment_configuration WHERE role = 'p' AND content = -1;
 select gp_request_fts_probe_scan();
 select gp_inject_fault('orc_writer_write_tuple','panic',dbid) FROM gp_segment_configuration WHERE role = 'p' AND content > -1;
+-- start_ignore
 -- failed because of fault injection
 insert into t_insert select generate_series(1,10);
 
--- start_ignore
 -- clear the fault inject, so the next insert will success.
 -- put the reset operation in ignore range
 select gp_inject_fault('orc_writer_write_tuple','reset',dbid) FROM gp_segment_configuration WHERE role = 'p' AND content > -1;
